88aba2a5c32a2ea180ae2e4da7134e2e8f371f59
Gideon <48917820+GideonIsBuilding@users.noreply.github.com>
2023-10-15 09:51:50 +0100

    made it human-readable
    Added the -h flag and removed the "KB" portion of the used and free space outputs to prevent redundancy and confusion with the newly converted space metrics

--- 3-assignment.sh
+++ 3-assignment.sh
@@ -8,13 +8,13 @@ fi
 mountpoint="$1"
 mountpoint -q "$mountpoint"
 if [ $? -eq 0 ]; then
-    OUTPUT=$(df --output=used,avail "$mountpoint" | tail -n 1)
+    OUTPUT=$(df -h --output=used,avail "$mountpoint" | tail -n 1)
     USED_SPACE=$(echo "$OUTPUT" | awk '{print $1}')
     FREE_SPACE=$(echo "$OUTPUT" | awk '{print $2}')
 
     echo "Mount point or filesystem '$mountpoint' is mounted"
-    echo "Used space: $USED_SPACE KB"
-    echo "Free space: $FREE_SPACE KB"
+    echo "Used space: $USED_SPACE"
+    echo "Free space: $FREE_SPACE"
 else
     echo "Mount point or filesystem '$mountpoint' is not mounted"
     exit 2
